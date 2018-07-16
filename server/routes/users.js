251876a3816ceb53633f010c0acb19bb8378b579
Alexander Stoyanov <relax2865@gmail.com>
2018-07-16 00:28:55 -0400

    Implemented pooling for the db

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,51 +1,45 @@
 import express from 'express';
 import validateInput from '../shared/validations/signup';
-
+import shortid from 'shortid';
+import pg from 'pg';
 let router = express.Router();
 
-
-var pg = require('pg');
-var conString = process.env.ELEPHANTSQL_URL || "	postgres://fxnccmwu:UVEOgDD08que1CZcC9L2twa6PxN11Cyo@baasu.db.elephantsql.com:5432/fxnccmwu";
+//Database connection
+var conString = process.env.ELEPHANTSQL_URL || "postgres://fxnccmwu:UVEOgDD08que1CZcC9L2twa6PxN11Cyo@baasu.db.elephantsql.com:5432/fxnccmwu";
 var client = new pg.Client(conString);
-client.connect();
 
-//client.query('CREATE TABLE people(id SERIAL PRIMARY KEY, username VARCHAR(40) not null, password VARCHAR(40) not null)');
+//Pooling
+import { Pool } from 'pg';
+const connectionString = 'postgres://fxnccmwu:UVEOgDD08que1CZcC9L2twa6PxN11Cyo@baasu.db.elephantsql.com:5432/fxnccmwu';
+const pool = new Pool({
+    connectionString: connectionString,
+});
 
-//const { Pool } = require('pg');
-//const pool = new Pool();
-//pool.on('error', (err, client) => {
-//    console.error('Unexpected error on idle client', err);
-//    process.exit(-1);
-//});
 
+//client.query('CREATE TABLE users(id VARCHAR(10) PRIMARY KEY, username VARCHAR(10) not null, password VARCHAR(40) not null)');
 
 router.post('/', (req, res) => {
     const { errors, isValid } = validateInput(req.body);
 
     if (isValid) {
-        res.json({ success: true });
-        client.connect();
         const query = {
-            text: 'INSERT INTO people(id, username, password) VALUES($1, $2, $3)',
-            values: ['1111', 'test1', 'test2@gmail.com'],
+            text: 'INSERT INTO users(id, username, password) VALUES($1, $2, $3)',
+            values: [shortid.generate(), req.body.username, req.body.password],
         }
 
-        // callback
-        client.query(query, (err, res) => {
-            if (err) {
-                console.log(err.stack)
-            } else {
-                console.log(res.rows[0])
-            }
-        })
+        pool.connect((err, client, done) => {
+            if (err) throw err
+            client.query(query, (err, res) => {
+                done();
 
-        //client.query('SELECT * FROM people', function (err, result) {
-        //    if (err) {
-        //        return console.error('error running query', err);
-        //    }
-        //    console.log(result.rows);
-        //    client.end();
-        //});
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    console.log(res);
+                }
+            });
+        });
+        res.json({ success: true });
     } else {
         res.status(400).json(errors);
     }
